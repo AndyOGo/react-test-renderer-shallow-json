09fbeacd29271e012bb0548b1ee7557b0e47f33f
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2023-04-19 22:39:20 +0200

    docs: improve readme and api docs

--- src/shallowJSON.ts
+++ src/shallowJSON.ts
@@ -2,6 +2,12 @@ import type { ReactTestRendererJSON } from 'react-test-renderer';
 import { ReactTestRendererTreeFixed } from './types';
 import { mapTrees } from './mapTrees';
 
+/**
+ * Return an object representing the rendered tree.
+ * This tree contains the platform-specific nodes like `<div>` or `<View>` and includes the user-written components as well.
+ * User-written components are rendered recursively up to the specified `depth`.
+ * This is handy for **shallow** [snapshot testing](https://facebook.github.io/jest/docs/en/snapshot-testing.html#snapshot-testing-with-jest).
+ */
 export function shallowJSON(
   trees: null | ReactTestRendererTreeFixed | ReactTestRendererTreeFixed[],
   depth = 1
